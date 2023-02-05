30ca59302b942b13dc233bc60ceb538a62d5a26c
Jeremiah Almazar <jemalmazar@gmail.com>
2023-02-04 20:09:12 -0800

    Build tab functionality

--- scripts.js
+++ scripts.js
@@ -3,6 +3,14 @@ const tabcordionButtons = tabcordion.querySelectorAll('[role="tab"]');
 const expandArrows = tabcordion.querySelectorAll('[role="tab"] span');
 const tabcordionPanels = Array.from(tabcordion.querySelectorAll('[role="tabpanel"]'));
 
+if (window.innerWidth >= 1080) {
+    tabcordionButtons[0].setAttribute('aria-expanded', true);
+    tabcordionButtons[0].style.borderTop = 'solid 3px black';
+    tabcordionButtons[0].style.borderBottom = 'none';
+    tabcordionPanels[0].setAttribute('aria-hidden', false);
+    tabcordionPanels[0].style.maxHeight = `${tabcordionPanels[0].scrollHeight}px`;
+}
+
 function handleClick(e) {
     // grab button panel's open or closed status
     const isTabOpen = e.currentTarget.getAttribute('aria-expanded');
@@ -14,6 +22,7 @@ function handleClick(e) {
     // mark all buttons as unselected
     tabcordionButtons.forEach((button) => {
         button.setAttribute('aria-expanded', false);
+        button.style = null;
     });
 
     // hide all panels & mark all as hidden
@@ -27,6 +36,8 @@ function handleClick(e) {
 
     if (isTabOpen === 'false') {
         e.currentTarget.setAttribute('aria-expanded', true);
+        e.currentTarget.style.borderTop = 'solid 3px black';
+        e.currentTarget.style.borderBottom = `none`;
         e.currentTarget.children[0].style.transform = 'rotate(180deg)';
         tabPanel.setAttribute('aria-hidden', false);
         // .scrollHeight interpolation into px string code from: https://dev.to/lizlaffitte/creating-an-accordion-with-html-css-javascript-3gmn
