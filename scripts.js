d4e57c0d09115a24c759a177a5afb280912f70be
Jeremiah Almazar <jemalmazar@gmail.com>
2023-03-01 15:27:58 -0800

    Build and style palindrome check form and write form logic

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ const tabcordion = document.querySelector('.tabcordion');
 const tabcordionButtons = tabcordion.querySelectorAll('[role="tab"]');
 const expandArrows = tabcordion.querySelectorAll('[role="tab"] span');
 const tabcordionPanels = Array.from(tabcordion.querySelectorAll('[role="tabpanel"]'));
+const panelForms = tabcordion.querySelectorAll('form');
+const resultDivs = Array.from(tabcordion.querySelectorAll('.result'));
 
-if (window.innerWidth >= 1080) {
-    tabcordionButtons[0].setAttribute('aria-expanded', true);
-    tabcordionButtons[0].style.borderTop = 'solid 3px black';
-    tabcordionButtons[0].style.borderBottom = 'none';
-    tabcordionPanels[0].setAttribute('aria-hidden', false);
-    tabcordionPanels[0].style.maxHeight = `${tabcordionPanels[0].scrollHeight}px`;
+function palindrome(str) {
+    const formattedStr = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    const reversedStr = formattedStr.split('').reverse().join('');
+    return formattedStr === reversedStr;
 }
 
 function handleClick(e) {
@@ -45,4 +45,37 @@ function handleClick(e) {
     }
 }
 
+function handleSubmit(e) {
+    e.preventDefault();
+    const { name } = e.currentTarget;
+    const targetDiv = resultDivs.find((panel) => panel.getAttribute('aria-labelledby') === name);
+
+    if (e.currentTarget.palindrome.value) {
+        if (palindrome(e.currentTarget.palindrome.value)) {
+            targetDiv.innerHTML = `
+                <p>
+                    <span class="material-symbols-outlined">check_circle</span>
+                    Palindrome
+                </p> 
+            `;
+        } else {
+            targetDiv.innerHTML = `
+                <p>
+                    <span class="material-symbols-outlined">cancel</span>
+                    Not a Palindrome
+                </p>
+            `;
+        }
+    }
+}
+
+if (window.innerWidth >= 1080) {
+    tabcordionButtons[0].setAttribute('aria-expanded', true);
+    tabcordionButtons[0].style.borderTop = 'solid 3px black';
+    tabcordionButtons[0].style.borderBottom = 'none';
+    tabcordionPanels[0].setAttribute('aria-hidden', false);
+    tabcordionPanels[0].style.maxHeight = `${tabcordionPanels[0].scrollHeight}px`;
+}
+
 tabcordionButtons.forEach((button) => button.addEventListener('click', handleClick));
+panelForms.forEach((form) => form.addEventListener('submit', handleSubmit));
